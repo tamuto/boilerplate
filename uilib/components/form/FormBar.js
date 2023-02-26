0f5a95c2a58899cbfddfde25da9e3f9cab036e2f
tamuto <mutomob@gmail.com>
2023-02-26 19:47:57 +0000

    css修正

--- uilib/components/form/FormBar.js
+++ uilib/components/form/FormBar.js
@@ -2,8 +2,8 @@ import styled from '@emotion/styled'
 
 const FormBar = styled.form`
   display: flex;
-  &> :not(:first-of-type) {
-    margin-left: 16px;
+  &> :not(:last-child) {
+    margin-right: 16px;
   }
 `
 
