b2b672b5fd307c8250661cd3ad412827966a01ee
tamuto <mutomob@gmail.com>
2023-02-26 19:53:23 +0000

    css修正

--- uilib/components/form/FormBar.js
+++ uilib/components/form/FormBar.js
@@ -2,9 +2,7 @@ import styled from '@emotion/styled'
 
 const FormBar = styled.form`
   display: flex;
-  &> :not(:last-child) {
-    margin-right: 16px;
-  }
+  gap: 20px 16px;
 `
 
 export default FormBar
